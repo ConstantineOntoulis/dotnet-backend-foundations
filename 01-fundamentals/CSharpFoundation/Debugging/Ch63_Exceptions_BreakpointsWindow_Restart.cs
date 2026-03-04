8311bebadf2c639499dac1a7e1867b027db4cf99
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-04 13:55:00 +0200

    Ch63: Create Issue by Reducing the size of the list

--- 01-fundamentals/CSharpFoundation/Debugging/Ch63_Exceptions_BreakpointsWindow_Restart.cs
+++ 01-fundamentals/CSharpFoundation/Debugging/Ch63_Exceptions_BreakpointsWindow_Restart.cs
@@ -18,9 +18,37 @@ namespace CSharpFoundation.Debugging
     {
         public static void Run()
         {
+            var numbers = new List<int> { 1, 2 }; // Remove the last 4 numbers so we only have 2. That will cause n "Out of Range" Exception
+            var smallests = GetSmallests(numbers, 3); 
 
+            foreach (var number in smallests)
+                Console.WriteLine(number);
 
             Console.ReadKey();
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var minim = GetSmallest(list);                     
+                smallests.Add(minim);
+                list.Remove(minim);
+            }
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            var min = list[0];
+
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
         }
     }
 }

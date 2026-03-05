7cb44c24e5098a969c47ea3e56779c9f083580f9
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-05 11:19:18 +0200

    Ch64: Creation of buffer list to prevent issues with the original list and introduction to a new problem for debugging

--- 01-fundamentals/CSharpFoundation/Debugging/Ch64_SideEffects_RemoveMutationWithBufferCopy.cs
+++ 01-fundamentals/CSharpFoundation/Debugging/Ch64_SideEffects_RemoveMutationWithBufferCopy.cs
@@ -15,9 +15,38 @@ namespace CSharpFoundation.Debugging
     {
         public static void Run()
         {
+            var numbers = new List<int> { 1, 2 }; // Remove the last 4 numbers so we only have 2. That will cause n "Out of Range" Exception
+            var smallests = GetSmallests(numbers, 3);
 
+            foreach (var number in smallests)
+                Console.WriteLine(number);
 
             Console.ReadKey();
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var buffer = new List<int>(list); // We create a new list to make sure that everything works properly 
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var minim = GetSmallest(buffer);
+                smallests.Add(minim);
+                buffer.Remove(minim); // Now, instead of removing the numbers from the original list, they are removed
+            }                         // from the buffer list
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            var min = list[0];
+
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
         }
     }
 }

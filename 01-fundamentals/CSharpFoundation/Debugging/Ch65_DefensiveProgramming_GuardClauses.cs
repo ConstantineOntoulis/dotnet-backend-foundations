abe3a8df9390e5a4c4922a152f6c5256dc87861f
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-05 12:46:16 +0200

    Ch65: Tuckling OutOfRangeException, added if statements in the GetSmallests method

--- 01-fundamentals/CSharpFoundation/Debugging/Ch65_DefensiveProgramming_GuardClauses.cs
+++ 01-fundamentals/CSharpFoundation/Debugging/Ch65_DefensiveProgramming_GuardClauses.cs
@@ -16,9 +16,44 @@ namespace CSharpFoundation.Debugging
     {
         public static void Run()
         {
-
+            var numbers = new List<int> { 1, 2 }; 
+            var smallests = GetSmallests(numbers, 3); // Client of method sends wrong data for unhandled exception
+            // if instead of numbers, we used null, we would get a no reference exception
+            foreach (var number in smallests)
+                Console.WriteLine(number);
 
             Console.ReadKey();
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count) // We need to enuse that the "count" is not
+        {                                                               // Greater than the items we receive in the list
+            //if (list == null)
+            //    throw new ArgumentNullException("list");
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of elements in the list.");
+            
+            var buffer = new List<int>(list); 
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var minim = GetSmallest(buffer);
+                smallests.Add(minim);
+                buffer.Remove(minim); 
+            }                        
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            var min = list[0]; // Failure point
+                               // To solve, we may either use a conditional statement here to enture
+                               // The list wil not be empty. It is wiser to use one in the GetSmallests method instead.
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
         }
     }
 }

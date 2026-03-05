9e7a88c3648853ebb766a158184cd2ffc45ad861
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-05 20:11:05 +0200

    Ch66: Solution initialization

--- 01-fundamentals/CSharpFoundation/Debugging/Ch66_CallStack_Navigation.cs
+++ 01-fundamentals/CSharpFoundation/Debugging/Ch66_CallStack_Navigation.cs
@@ -15,9 +15,41 @@ namespace CSharpFoundation.Debugging
     {
         public static void Run()
         {
-
+            var numbers = new List<int> { 1, 2 };
+            var smallests = GetSmallests(numbers, 3); 
+            foreach (var number in smallests)
+                Console.WriteLine(number);
 
             Console.ReadKey();
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {                                                              
+            //  if (list == null)
+            //        throw new ArgumentNullException("list");
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of elements in the list.");
+
+            var buffer = new List<int>(list);
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var minim = GetSmallest(buffer);
+                smallests.Add(minim);
+                buffer.Remove(minim);
+            }
+            return smallests;
+        }
+        public static int GetSmallest(List<int> list)
+        {
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
         }
     }
 }

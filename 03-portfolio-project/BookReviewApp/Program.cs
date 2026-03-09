fc8e4bca9f6c75d700996e384f3127f7b774f683
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-09 12:39:55 +0200

    Connecting Database to the program

--- 03-portfolio-project/BookReviewApp/Program.cs
+++ 03-portfolio-project/BookReviewApp/Program.cs
@@ -1,6 +1,11 @@
+using BookReviewApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddOpenApi();
 

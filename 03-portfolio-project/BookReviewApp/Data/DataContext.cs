cae3b30d7aee1dc7182e680dd18aa44f5a8fd774
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-09 12:32:42 +0200

    Database Map for BookReviewApp creation and Onmodel Creating

--- 03-portfolio-project/BookReviewApp/Data/DataContext.cs
+++ 03-portfolio-project/BookReviewApp/Data/DataContext.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookReviewApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookReviewApp.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions) { }
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // Method that allows use to customize how EntityFramework builds tables and relationships
+        {
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(ba => new { ba.BookId, ba.AuthorId });
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(bc => new {bc.BookId, bc.CategoryId});
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }

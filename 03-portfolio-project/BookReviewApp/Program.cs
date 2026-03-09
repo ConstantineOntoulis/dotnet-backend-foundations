7a6d5d347f5672d425b6e770f60dd58d76be6e29
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-09 17:52:13 +0200

    Seeding setup complete

--- 03-portfolio-project/BookReviewApp/Program.cs
+++ 03-portfolio-project/BookReviewApp/Program.cs
@@ -1,9 +1,12 @@
+using BookReviewApp;
 using BookReviewApp.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+// This means that every time the application requests a seed instance it will create a new one
+builder.Services.AddTransient<Seed>(); 
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -21,5 +24,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// call class within program.cs to populate the Database automatically when the API starts
+using (var scope = app.Services.CreateScope())
+{
+    var seed = scope.ServiceProvider.GetRequiredService<Seed>();
+    seed.SeedDataContext();
+}
 
 app.Run();

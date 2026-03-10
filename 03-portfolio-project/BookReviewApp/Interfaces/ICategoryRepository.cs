abf3f7059565bf2feb5b42a9ae2c78d046197287
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-10 21:00:54 +0200

    ICategoryRepository Setup

---
+++ 03-portfolio-project/BookReviewApp/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookReviewApp.Models;
+
+namespace BookReviewApp.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int categoryId);
+        ICollection<Book> GetBooksByCategory(int categoryId);
+        bool CategoryExists(int categoryId);
+    }
+}

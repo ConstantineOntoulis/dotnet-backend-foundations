aa3f363ff12a6331b56c55aaacf78d022d32df28
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-10 13:11:00 +0200

    Interface Method Definitions Addition

--- 03-portfolio-project/BookReviewApp/Interfaces/IBookRepository.cs
+++ 03-portfolio-project/BookReviewApp/Interfaces/IBookRepository.cs
@@ -5,5 +5,9 @@ namespace BookReviewApp.Interfaces
     public interface IBookRepository
     {
         ICollection<Book> GetAllBooks();
+        Book GetBook(int id);
+        Book GetBookById(int id);
+        decimal GetBookRating(int bookId);
+        bool BookExists(int bookId);
     }
 }

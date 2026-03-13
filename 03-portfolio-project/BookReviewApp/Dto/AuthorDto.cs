3f246b449c0c2b891a00f75af0bd30530631e36f
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-13 15:03:18 +0200

    AuthorDto Created

---
+++ 03-portfolio-project/BookReviewApp/Dto/AuthorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookReviewApp.Dto
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+    }
+}

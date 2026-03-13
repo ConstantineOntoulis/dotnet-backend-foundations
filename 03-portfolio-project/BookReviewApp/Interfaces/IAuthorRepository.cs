303a0ae2b569e23577f72e84c56136806672e715
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-13 14:37:44 +0200

    AuthorRepository Interface Created

---
+++ 03-portfolio-project/BookReviewApp/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookReviewApp.Models;
+
+namespace BookReviewApp.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        ICollection<Author> GetAllAuthors();
+        Author GetAuthorById(int AuthorId);
+        ICollection<Author> GetAuthorOfBooks(int bookId);
+        ICollection<Book> GetBooksByAuthor(int authorId);
+        bool AuthorExists(int authorId);
+    }
+}

957e42bb1cb9f25eb620ad69d485c79e53477773
Constantine Ontoulis <konstantinosontoulis@gmail.com>
2026-03-13 15:33:01 +0200

    Added Create, Delete and Save functions to CategoryRepository Interface

--- 03-portfolio-project/BookReviewApp/Interfaces/ICategoryRepository.cs
+++ 03-portfolio-project/BookReviewApp/Interfaces/ICategoryRepository.cs
@@ -7,6 +7,10 @@ namespace BookReviewApp.Interfaces
         ICollection<Category> GetCategories();
         Category GetCategory(int categoryId);
         ICollection<Book> GetBooksByCategory(int categoryId);
+        bool UpdateCategory(Category category);
         bool CategoryExists(int categoryId);
+        bool CreateCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool Save();
     }
 }
